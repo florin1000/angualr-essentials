644c158f54932b009e9d87bb342f8cf21c8251c4
Florin <oancea.quentin.florin@gmail.com>
2020-06-24 18:28:29 +0300

    View Child example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-essentials';
+  @ViewChild('ref', { static: false },) ref: ElementRef;
+
+
+  ngAfterViewInit(): void {
+    console.log(this.ref);
+  }
 }
